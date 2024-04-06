ef5ea9f720c6552a53ce990a65b8e0cfad8f3786
torufuruya <t.furuya825@gmail.com>
2024-04-06 10:56:51 +0900

    feat: Add privacy policy

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -4,7 +4,7 @@ import { HttpStatusCode } from "solid-start/server";
 export default function NotFound() {
   return (
     <main>
-      <Title>Not Found</Title>
+      <Title>Matchday</Title>
       <HttpStatusCode code={404} />
       <h1>Page Not Found</h1>
     </main>
---
+++ src/routes/privacypolicy.tsx
@@ -0,0 +1,12 @@
+import { Title } from "solid-start";
+
+export default function Home() {
+  return (
+    <main>
+      <Title>Matchday</Title>
+      <h1>Privacy Policy</h1>
+      <p>WIP</p>
+      <a href="https://docs.google.com/document/d/1ZpLSfakOQwa65CQ6s6sGYZJ8-PR5DajwOKEAJlxOdXs/preview">Provisional Version</a>
+    </main>
+  );
+}
