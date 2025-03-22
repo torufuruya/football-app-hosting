3f75dba19ff4eecdcf590bb49be237cf2abc8266
torufuruya <t.furuya825@gmail.com>
2025-03-22 16:17:37 +0900

    feat: Update root design

--- src/root.tsx
+++ src/root.tsx
@@ -18,16 +18,22 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - Bare</Title>
+        <Title>Matchday App</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Body>
         <Suspense>
           <ErrorBoundary>
-            <A href="/">Index</A>
-            <A href="/privacypolicy">Privacy Policy</A>
-            <A href="/termsofservice">Terms of Service</A>
+            <header>
+              <nav>
+                <A href="/" class="nav-brand">Matchday</A>
+                <div class="nav-links">
+                  <A href="/privacypolicy">Privacy Policy</A>
+                  <A href="/termsofservice">Terms of Service</A>
+                </div>
+              </nav>
+            </header>
             <Routes>
               <FileRoutes />
             </Routes>
